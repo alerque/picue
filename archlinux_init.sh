960126e0ad5c8ba28a56d59fd7fc0b123a1e5c50
Caleb Maclennan <caleb@alerque.com>
2012-12-21 15:37:37 +0200

    Add VBox support.

--- archlinux_init.sh
+++ archlinux_init.sh
@@ -4,11 +4,21 @@ echo picue > /etc/hostname
 ln -sf /usr/share/zoneinfo/Turkey /etc/localtime
 
 pacman --noconfirm -Syu
+pacman-key --init
 pacman --noconfirm -S xorg-server xorg-xinit xorg-server-utils xf86-video-fbdev mesa
 pacman --noconfirm -S alsa-firmware alsa-utils alsa-oss
-pacman --noconfirm -S vim sudo awesome
+pacman --noconfirm -S vim sudo awesome git
+
+if lspci | grep -c VirtualBox; then
+	pacman --noconfirm -S openssh virtualbox-guest-utils
+	modprobe -a vboxguest vboxsf vboxvideo
+	echo "vboxguest
+	vboxsf
+	vboxvideo" > /etc/modules-load.d/virtualbox.conf
+	# echo 'VBoxClient-all &' > .xinitrc
+fi
 
 if [ ! -f /usr/bin/rpi-update ]; then
-	wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update && chmod +x /usr/bin/rpi-update
+	curl http://goo.gl/1BOfJ > /usr/bin/rpi-update && chmod +x /usr/bin/rpi-update
 	rpi-update
 fi
