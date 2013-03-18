41ac6d40a2e8f7a7393100de7d002c37fd5c7a47
Caleb Maclennan <caleb@alerque.com>
2013-03-19 01:41:51 +0200

    Added functin that has parts to expland file system (not auto run)

--- archlinux_init.sh
+++ archlinux_init.sh
@@ -10,6 +10,24 @@ if [ -f "/var/lib/pacman/db.lck" ]; then
 	exit
 fi
 
+# TODO: check and if possible grow main partition
+do_expand_rootfs() {
+	#SOURCE: https://github.com/asb/raspi-config/blob/master/raspi-config
+  fdisk /dev/mmcblk0 <<EOF
+p
+d
+2
+n
+p
+2
+
+
+p
+w
+EOF
+resize2fs /dev/mmcblk0p2
+}
+
 pacman --noconfirm -Syu
 pacman-key --init
 pacman --noconfirm -S xorg-server xorg-xinit xorg-server-utils xf86-video-fbdev mesa xf86-input-evdev
